d207fa1393225fe9f38b2e46ed9f583b04f300b8
Gautam <aditya.gautam@dxc.com>
2018-12-12 15:02:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as appName} from './index.json';
+
+AppRegistry.registerComponent(appName, () => App);
